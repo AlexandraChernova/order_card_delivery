84b4ebd1840b3b2d36ee6093b021cf628204c50b
Alexandra Chernova <chernova.aleksandra28@gmail.com>
2023-01-28 17:46:55 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/OrderCardDeliveryTest.java
@@ -0,0 +1,53 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class OrderCardDeliveryTest {
+    @BeforeEach
+    void setup(){ open("http://localhost:9999/");
+    }
+
+    @Test
+    @DisplayName("Should successful set the meeting day ")
+    void shouldSuccessfulSetTheMeetingDay() {
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int dayOfFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(dayOfFirstMeeting);
+        int dayOfSecondMeeting = 7;
+        String secondMeetingDate = DataGenerator.generateDate(dayOfSecondMeeting);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue("+79201666666");//метод генератор
+        $("[data-test-id='agreement']").click();
+        $("button.button").click();
+        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
+        $(byText("Перепланировать")).click();
+
+
+
+
+
+
+
+    }
+}
